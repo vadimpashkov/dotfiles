09df49c4df18e833d2f5eb9471b872c730edeff4
Vadim Pashkov <vadimpashkov.job@gmail.com>
2024-06-02 16:50:02 +0300

    (nvim) Add new mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,3 +1,12 @@
 -- Назначаем Space как <leader>
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+-- Выход из INSERT мода
+vim.keymap.set('i', 'jj', '<Esc>')
+-- Тоже самое делаем и для русского языка, ибо Vim не умеет работать с кириллицей
+vim.keymap.set('i', 'оо', '<Esc>')
+
+-- Сохранить изменения в файле  
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Сохранить файл" })vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Сохранить" })
+vim.keymap.set("n", "<leader>c", ":bd<CR>", { desc = "Закрыть файл" })
