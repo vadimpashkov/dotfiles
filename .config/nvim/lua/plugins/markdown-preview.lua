bb3b200f50a9573fefd0bf0f6f4d742910c3231b
Vadim Pashkov <vadimpashkov.job@gmail.com>
2024-06-04 17:35:27 +0300

    (nvim) add markdown-preview lab

---
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,16 @@
+return {
+	"iamcco/markdown-preview.nvim",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	ft = { "markdown" },
+	build = function()
+		vim.fn["mkdp#util#install"]()
+	end,
+	config = function()
+		vim.keymap.set(
+			"n",
+			"<leader>md",
+			":MarkdownPreviewToggle<CR>",
+			{ desc = "Открыть/закрыть просмотрщик .md файлов" }
+		)
+	end,
+}
